e3156499b596d8e7310ce8cfb971a1572058cd05
nishchal05 <nishchalsundan04@gmail.com>
2026-02-07 23:51:28 +0530

    update

--- routes/api.js
+++ routes/api.js
@@ -54,6 +54,7 @@ router.put('/users', async (req, res) => {
                             if (exp.type !== undefined) existingExpense.type = exp.type;
                             if (exp.payingEntity !== undefined) existingExpense.payingEntity = exp.payingEntity;
                             if (exp.confirmation !== undefined) existingExpense.confirmation = exp.confirmation;
+                            if (exp.category !== undefined) existingExpense.category = exp.category;
                         } else {
                             // Add new expense with index
                             user.expenses.push(exp);
